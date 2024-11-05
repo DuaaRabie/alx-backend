1860e2c2d8bac631c7c63b3f3e7995e19328b115
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-05 17:35:44 +0000

    update code

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,16 @@
 """ Basic Flask app """
 
 
-from flask import Flask
-from flask_babel import Babel
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
-app.config.from_pyfile('mysettings.cfg')
-babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ Route for the home page """
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
