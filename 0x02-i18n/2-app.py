f133f3b5f99765083285811f5a668bd81af1eff4
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-05 19:28:02 +0000

    update code

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 
 
 @app.route('/')
