aa3a6e0b8af5eee3b06a86d775c7027ccc9db5ab
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-05 19:10:41 +0000

    update code

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,7 +3,8 @@
 
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
+
 
 app = Flask(__name__)
 
@@ -18,10 +19,11 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     """ Determine the best language """
-     return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
